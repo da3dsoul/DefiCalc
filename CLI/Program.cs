4ffd20e88ab42d534856a36baf74f623a50f8278
da3dsoul <da3dsoul@gmail.com>
2022-06-13 23:21:30 -0400

    Improve Output

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -15,19 +15,13 @@ namespace DefiCalc.CLI
             {
                 new()
                 {
-                    Amount = 1000,
-                    StartDate = new DateTime(2022, 6, 14),
-                    EndDate = new DateTime(2022, 6, 15)
-                },
-                new()
-                {
                     Amount = 500,
                     StartDate = new DateTime(2022, 7, 12),
                     Period = 28
                 },
                 new()
                 {
-                    Amount = 500,
+                    Amount = 480,
                     StartDate = new DateTime(2022, 8, 9),
                     Period = 14
                 }
@@ -39,17 +33,17 @@ namespace DefiCalc.CLI
                 {
                     if (cliArgs.InitialPrinciple < 500)
                         Console.WriteLine(
-                            "Day: {0:D3}, Interest: {1:P1}, PV*I: ${2:N2}, Pending Withdraw: ${3:N2}, Amount Withdrawn: ${4:N2}, Total: ${5:N2}",
-                            eventArgs.Day, eventArgs.InterestRate, eventArgs.AmountToAdd,
+                            "Date: {0:yyyy-MM-dd}, Interest: {1:P1}, PV*I: ${2:N2}, Pending Withdraw: ${3:N2}, Amount Withdrawn: ${4:N2}, Total: ${5:N2}",
+                            DateTime.Today.AddDays(eventArgs.Day), eventArgs.InterestRate, eventArgs.AmountToAdd,
                             eventArgs.AmountPendingWithdraw,
                             eventArgs.AmountWithdrawn, eventArgs.Total);
                     else
                     {
                         if (schedule.Count > 0)
-                            Console.WriteLine("Day: {0:D3}, Interest: {1:P1}, PV*I: ${2:N2}, Additional Investment: ${3:N2}, Total: ${4:N2}", eventArgs.Day,
+                            Console.WriteLine("Date: {0:yyyy-MM-dd}, Interest: {1:P1}, PV*I: ${2:N2}, Additional Investment: ${3:N2}, Total: ${4:N2}", DateTime.Today.AddDays(eventArgs.Day),
                                 eventArgs.InterestRate, eventArgs.AmountToAdd, eventArgs.AdditionalInvestment, eventArgs.Total);
                         else
-                            Console.WriteLine("Day: {0:D3}, Interest: {1:P1}, PV*I: ${2:N2}, Total: ${3:N2}", eventArgs.Day,
+                            Console.WriteLine("Date: {0:yyyy-MM-dd}, Interest: {1:P1}, PV*I: ${2:N2}, Total: ${3:N2}", DateTime.Today.AddDays(eventArgs.Day),
                                 eventArgs.InterestRate, eventArgs.AmountToAdd, eventArgs.Total);
                     }
                 };
@@ -63,13 +57,24 @@ namespace DefiCalc.CLI
                         Console.WriteLine("------------{0:yyyy-MM-dd}-------------", date);
                 };
 
-                Console.WriteLine("-----Initial Values----------------");
+                Console.WriteLine("-----------Settings----------------");
                 Console.WriteLine("Days: {0}", cliArgs.Days);
-                Console.WriteLine("Reinvestment Period: {0} days", cliArgs.ReinvestmentPeriod);
-                Console.WriteLine("Reinvestment Offset: {0} days", cliArgs.ReinvestmentOffset);
-                Console.WriteLine("Reinvestment Amount: ${0:N2}", cliArgs.ReinvestmentAmount);
                 Console.WriteLine("Initial Principle (PV): ${0:N2}", cliArgs.InitialPrinciple);
-                Console.WriteLine("-----------------------------------");
+                if (schedule.Count > 0)
+                    Console.WriteLine("-----------Schedule----------------");
+                else
+                    Console.WriteLine("-----------------------------------");
+                foreach (var investmentSchedule in schedule)
+                {
+                    Console.WriteLine("  Amount: ${0:N2}", investmentSchedule.Amount);
+                    Console.WriteLine("  Start Date: {0:yyyy-MM-dd}", investmentSchedule.StartDate);
+                    if (investmentSchedule.Period != 0)
+                        Console.WriteLine("  Period: {0} days", investmentSchedule.Period);
+                    if (investmentSchedule.EndDate != null)
+                        Console.WriteLine("  End Date: {0:yyyy-MM-dd}", investmentSchedule.EndDate);
+                    Console.WriteLine("-----------------------------------");
+                }
+
                 var total = Calc.Calc(cliArgs.Days, cliArgs.InitialPrinciple, schedule);
                 Console.WriteLine("-----------------------------------");
                 Console.WriteLine("Total: ${0:N2}", total);
